b0c7f341b4eae7dc70951978567a49154ec0097a
stef <pitchfork@ctrlc.hu>
2013-12-08 19:38:19 +0100

    [mod] systimer cosmetics

--- core/systimer.c
+++ core/systimer.c
@@ -5,15 +5,13 @@ void systick_handler(void);
 volatile unsigned long long sysctr;
 volatile unsigned int TimingDelay ;
 
-void sys_tick_handler(void)
-{
+void sys_tick_handler(void) {
   sysctr++;
   if ( TimingDelay != 0x00)
     TimingDelay --;
 }
 
-void systick_init(void)
-{
+void systick_init(void) {
   SYSTICK_LOAD = SYSCLCK / 1000; /* Frequency of 1 kHz (ms)*/
   SYSTICK_CTRL |= 4; /* use sysclck */
   SYSTICK_CTRL |= 1; /* Enable Counter */
