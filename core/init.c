bd1c149fc34d1ad8b104556bffab468043779dd6
stef <pitchfork@ctrlc.hu>
2013-12-08 19:36:35 +0100

    [enh] added irqs for sdio&usb

--- core/init.c
+++ core/init.c
@@ -5,13 +5,21 @@
 #include "rng.h"
 #include "adc.h"
 #include "systimer.h"
+#include "irq.h"
+#include "sd.h"
+#include "led.h"
+#include "keys.h"
 
 void init(void) {
   clock_init();
+  led_init();
   uart_init();
   rnd_init();
   adc_init();
   sysctr = 0;
   systick_init();
+  irq_init();
+  SD_Init();
   usb_init();
+  keys_init();
 }
