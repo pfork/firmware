40e0ab674e3d911f195d23a11474758f1a5b737e
stef <pitchfork@ctrlc.hu>
2013-12-11 01:26:20 +0100

    [mod] removed some defs from usbd_def

--- usb/msc/usbd_def.h
+++ usb/msc/usbd_def.h
@@ -39,18 +39,6 @@
 #define  USB_REQ_RECIPIENT_ENDPOINT                     0x02
 #define  USB_REQ_RECIPIENT_MASK                         0x03
 
-#define  USB_REQ_GET_STATUS                             0x00
-#define  USB_REQ_CLEAR_FEATURE                          0x01
-#define  USB_REQ_SET_FEATURE                            0x03
-#define  USB_REQ_SET_ADDRESS                            0x05
-#define  USB_REQ_GET_DESCRIPTOR                         0x06
-#define  USB_REQ_SET_DESCRIPTOR                         0x07
-#define  USB_REQ_GET_CONFIGURATION                      0x08
-#define  USB_REQ_SET_CONFIGURATION                      0x09
-#define  USB_REQ_GET_INTERFACE                          0x0A
-#define  USB_REQ_SET_INTERFACE                          0x0B
-#define  USB_REQ_SYNCH_FRAME                            0x0C
-
 #define  USB_DESC_TYPE_DEVICE                              1
 #define  USB_DESC_TYPE_CONFIGURATION                       2
 #define  USB_DESC_TYPE_STRING                              3
