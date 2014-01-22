e749ef33611ac81f74ec0f546fc7d3348378a826
stef <pitchfork@ctrlc.hu>
2014-01-22 02:20:24 +0100

    [mod] removed get_entropy from rng.h

--- core/rng.h
+++ core/rng.h
@@ -5,5 +5,4 @@
 void rnd_init ( void );
 unsigned int next_rand ( void );
 unsigned int rand_read ( const size_t size, unsigned char* buf);
-unsigned int get_entropy ( const size_t size, unsigned char* buf);
 #endif
