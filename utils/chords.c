3b249edee3f75f0c0b10723a7b0dd2359056385b
stef <pitchfork@ctrlc.hu>
2016-05-15 20:34:17 +0200

    [mod] updated chords.c to 5 key board

--- utils/chords.c
+++ utils/chords.c
@@ -2,23 +2,22 @@
 #include "pitchfork.h"
 #include "keys.h"
 #include "oled.h"
+#include "delay.h"
 #include "ntohex.h"
 
-extern void (*app)(void);
-
-static uint8_t prev=0;
 static int n=0, i=0;
+static char prev;
 
 void chord_reset(void) {
   extern unsigned char outbuf[crypto_secretbox_NONCEBYTES+crypto_secretbox_ZEROBYTES+BUF_SIZE];
   uint8_t *outptr = outbuf, *outptr2 = outbuf+1024; // no bounds check careful!
-  n=0; i=0;
+  n=0; i=0; prev=0;
   outptr[0]=0;
   outptr2[0]=0;
   oled_print(0,0,"train chords",Font_8x8);
-  oled_print(0,16,"737466",Font_8x8);
+  oled_print(0,16,"5049544348464f524b",Font_8x8);
   oled_print(0,48,"press any key",Font_8x8);
-  oled_print(0,56,"long to exit",Font_8x8);
+  oled_print(0,56,"long + to exit",Font_8x8);
 }
 
 int chord_train(void) {
@@ -27,28 +26,35 @@ int chord_train(void) {
   uint8_t *outptr = outbuf, *outptr2 = outbuf+1024; // no bounds check careful!
   str[1]=0;
   keys = keys_pressed();
-  if(prev!=keys) {
-    ntohex(str, keys);
-    oled_print(60,24,(char*) str,Font_8x8);
-    n=0;
-    prev=keys;
-    return 1;
-  }
-  n++;
-  if(keys!=0) {
-    if(n==10000) {
-      // accept entry
-      outptr2[i/2] |= keys << (((i+1)%2)*4);
-      outptr2[(i/2)+1] =0;
-      outptr[i++] = str[0];
-      outptr[i] =0;
-      oled_print(00,32,(char*) outptr,Font_8x8);
-      oled_print(00,40,(char*) outptr2,Font_8x8);
-    }
-    if(n>40000) {
-      oled_clear();
+  if((keys & BUTTON_ENTER)!=0) {
+    n++;
+    if(n>400) {
+      i=0;
       return 0;
     }
+  } else {
+    n=0;
   }
+
+  ntohex(str, keys&0xf);
+  oled_print(60,24,(char*) str,Font_8x8);
+
+  if(i%2==0) {
+    outptr2[i/2] = (outptr2[i/2] & 0xf) | ((keys&0xf) << 4);
+  } else {
+    outptr2[i/2] = (outptr2[i/2] & 0xf0) | (keys&0xf);
+  }
+  outptr2[(i/2)+1] =0;
+  outptr[i] = str[0];
+  outptr[i+1] =0;
+  oled_print(00,32,(char*) outptr+((i>=16)*(i-15)),Font_8x8);
+  oled_print(00,40,(char*) outptr2+((i>=32)*(i/2-15)),Font_8x8);
+
+  if(keys & BUTTON_ENTER && prev!=keys) {
+    // accept entry
+    i++;
+    mDelay(200);
+  }
+  prev=keys;
   return 1;
 }
