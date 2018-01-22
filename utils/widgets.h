18f097d080bedca047e2c04ee7a768261c66a70b
stef <pitchfork@ctrlc.hu>
2018-01-22 22:10:08 +0100

    [mod] moved chord as get_passcode to widgets.h

--- utils/widgets.h
+++ utils/widgets.h
@@ -1,6 +1,9 @@
 #ifndef WIDGETS_H
 #define WIDGETS_H
 
+#include <stdint.h>
+#include <stddef.h>
+
 typedef struct {
   unsigned char idx;
   unsigned short top;
@@ -10,6 +13,7 @@ extern uint8_t gui_refresh;
 
 void getstr(char *prompt, uint8_t *name, int *len);
 int menu(MenuCtx *ctx, const uint8_t *menuitems[], const size_t menulen, void fn(char));
+int get_passcode(uint8_t *dst, unsigned int size);
 void statusline(void);
 
 #endif // WIDGETS_H
